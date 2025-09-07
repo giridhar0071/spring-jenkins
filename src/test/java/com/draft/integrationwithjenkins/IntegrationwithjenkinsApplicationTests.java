bf1e5ad92543c5caf7fe8201a3514e47c30bb998
giridhar0071 <giridharbandla@gmail.com>
2025-09-07 01:19:07 -0500

    initial commit

---
+++ src/test/java/com/draft/integrationwithjenkins/IntegrationwithjenkinsApplicationTests.java
@@ -0,0 +1,20 @@
+package com.draft.integrationwithjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class IntegrationwithjenkinsApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(IntegrationwithjenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test is executing");
+		assertEquals(true,true);
+	}
+
+}
